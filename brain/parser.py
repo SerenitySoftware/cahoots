eae08fdad56682a58c6e6374c76b278a3be1ca38
Ryan Vennell <ryan.vennell@gmail.com>
2012-06-18 23:14:10 -0500

    case 10: Added multithreading support for concurrent parser execution. This is officially the first multi-threaded code I've written...and python makes it sooooo easy.

--- brain/parser.py
+++ brain/parser.py
@@ -1,7 +1,9 @@
 #import pyparsing
 from parsers import address, base, boolean, character, date, email, equation, grammar, measurement, number, phone, place, programming, uri
 import datetime
+import threading
 
+# These are all the parser modules we want to test against
 checks = [
 	number.NumberParser,
 	character.CharacterParser,
@@ -17,17 +19,48 @@ checks = [
 	equation.EquationParser,
 ]
 
+
+class parserThread (threading.Thread):
+	"""
+	Represents a thread that will handle one parser parsing request
+	"""
+
+	parser = None
+	dataString = None
+	kwargs = None
+	result = None
+
+	def __init__(self, module, dataString, **kwargs):
+		self.threadId = module
+		self.dataString = dataString
+		self.kwargs = kwargs
+		threading.Thread.__init__(self)
+
+	def run(self):
+		self.parser = self.threadId()
+		self.result = self.parser.parse(self.dataString, **self.kwargs)
+
+
 def parse(dataString, *args, **kwargs):
 	match_types = []
 	results = []
-	
+	threads = []
+
+	# Creating/starting a thread for each parser module
 	for module in checks:
-		p = module()
-		result = p.parse(dataString, **kwargs)
-		
-		if result.Matched:
-			match_types.append(p.Type)
-			results.append(result)
+		thread = parserThread(module, dataString, **kwargs)
+		thread.start()
+		threads.append(thread)
+
+	# Sychronizing/finishing parser threads
+	for t in threads:
+		t.join()
+
+	# The threads are done, let's get the results out of them
+	for t in threads:
+		if t.result.Matched:
+			match_types.append(t.parser.Type)
+			results.append(t.result)
 			
 	matches = sorted(results, key = lambda result: result.Confidence, reverse = True)
 	match_count = len(matches)
