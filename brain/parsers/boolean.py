cd85adf220f17fff6161519e55f567187e5ffd32
Ryan Vennell <ryan.vennell@gmail.com>
2012-06-30 23:19:55 -0500

    case 5: Yup...

--- brain/parsers/boolean.py
+++ brain/parsers/boolean.py
@@ -11,11 +11,10 @@ class BooleanParser(BaseParser):
 		if data in ["true", "false", "yes", "no"]:
 			return self.result(True, "Boolean", 95)
 			
-		if data in ["yep", "nope"]:
+		if data in ["yep", "yup", "nope"]:
 			return self.result(True, "Boolean", 80)
 			
 		if data in ["1", "0", "t", "f", "one", "zero"]:
 			return self.result(True, "Boolean", 40)
 			
 		return self.result(False)
-		
