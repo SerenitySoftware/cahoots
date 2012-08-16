c07caab05f3c75fe6db8e23255ff10727a1ecf87
Jordan Ambra <jordan.ambra@gmail.com>
2012-08-15 23:42:20 -0400

    Typo on port binding

--- wsgi.py
+++ wsgi.py
@@ -88,4 +88,4 @@ def classifierApi():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8000), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8000)), debug=True)
