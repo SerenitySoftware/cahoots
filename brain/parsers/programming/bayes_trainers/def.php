48c46f6acc2fe79bae9f50036f6358ded866009b
Hickeroar <ryan.vennell@gmail.com>
2014-07-13 05:32:42 -0500

    Adding some php code with namespace usage to the trainer data.

--- brain/parsers/programming/bayes_trainers/def.php
+++ brain/parsers/programming/bayes_trainers/def.php
@@ -1,5 +1,410 @@
 <?php
 /**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\EventManager;
+
+use ArrayAccess;
+
+/**
+ * Representation of an event
+ *
+ * Encapsulates the target context and parameters passed, and provides some
+ * behavior for interacting with the event manager.
+ */
+class Event implements EventInterface
+{
+    /**
+     * @var string Event name
+     */
+    protected $name;
+
+    /**
+     * @var string|object The event target
+     */
+    protected $target;
+
+    /**
+     * @var array|ArrayAccess|object The event parameters
+     */
+    protected $params = array();
+
+    /**
+     * @var bool Whether or not to stop propagation
+     */
+    protected $stopPropagation = false;
+
+    /**
+     * Constructor
+     *
+     * Accept a target and its parameters.
+     *
+     * @param  string $name Event name
+     * @param  string|object $target
+     * @param  array|ArrayAccess $params
+     */
+    public function __construct($name = null, $target = null, $params = null)
+    {
+        if (null !== $name) {
+            $this->setName($name);
+        }
+
+        if (null !== $target) {
+            $this->setTarget($target);
+        }
+
+        if (null !== $params) {
+            $this->setParams($params);
+        }
+    }
+
+    /**
+     * Get event name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the event target
+     *
+     * This may be either an object, or the name of a static method.
+     *
+     * @return string|object
+     */
+    public function getTarget()
+    {
+        return $this->target;
+    }
+
+    /**
+     * Set parameters
+     *
+     * Overwrites parameters
+     *
+     * @param  array|ArrayAccess|object $params
+     * @return Event
+     * @throws Exception\InvalidArgumentException
+     */
+    public function setParams($params)
+    {
+        if (!is_array($params) && !is_object($params)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'Event parameters must be an array or object; received "%s"', gettype($params)
+            ));
+        }
+
+        $this->params = $params;
+        return $this;
+    }
+
+    /**
+     * Get all parameters
+     *
+     * @return array|object|ArrayAccess
+     */
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+    /**
+     * Get an individual parameter
+     *
+     * If the parameter does not exist, the $default value will be returned.
+     *
+     * @param  string|int $name
+     * @param  mixed $default
+     * @return mixed
+     */
+    public function getParam($name, $default = null)
+    {
+        // Check in params that are arrays or implement array access
+        if (is_array($this->params) || $this->params instanceof ArrayAccess) {
+            if (!isset($this->params[$name])) {
+                return $default;
+            }
+
+            return $this->params[$name];
+        }
+
+        // Check in normal objects
+        if (!isset($this->params->{$name})) {
+            return $default;
+        }
+        return $this->params->{$name};
+    }
+
+    /**
+     * Set the event name
+     *
+     * @param  string $name
+     * @return Event
+     */
+    public function setName($name)
+    {
+        $this->name = (string) $name;
+        return $this;
+    }
+
+    /**
+     * Set the event target/context
+     *
+     * @param  null|string|object $target
+     * @return Event
+     */
+    public function setTarget($target)
+    {
+        $this->target = $target;
+        return $this;
+    }
+
+    /**
+     * Set an individual parameter to a value
+     *
+     * @param  string|int $name
+     * @param  mixed $value
+     * @return Event
+     */
+    public function setParam($name, $value)
+    {
+        if (is_array($this->params) || $this->params instanceof ArrayAccess) {
+            // Arrays or objects implementing array access
+            $this->params[$name] = $value;
+        } else {
+            // Objects
+            $this->params->{$name} = $value;
+        }
+        return $this;
+    }
+
+    /**
+     * Stop further event propagation
+     *
+     * @param  bool $flag
+     * @return void
+     */
+    public function stopPropagation($flag = true)
+    {
+        $this->stopPropagation = (bool) $flag;
+    }
+
+    /**
+     * Is propagation stopped?
+     *
+     * @return bool
+     */
+    public function propagationIsStopped()
+    {
+        return $this->stopPropagation;
+    }
+}
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\ProgressBar;
+
+use Zend\Session;
+
+/**
+ * Zend\ProgressBar offers an interface for multiple environments.
+ */
+class ProgressBar
+{
+    /**
+     * Min value
+     *
+     * @var float
+     */
+    protected $min;
+
+    /**
+     * Max value
+     *
+     * @var float
+     */
+    protected $max;
+
+    /**
+     * Current value
+     *
+     * @var float
+     */
+    protected $current;
+
+    /**
+     * Start time of the progressbar, required for ETA
+     *
+     * @var int
+     */
+    protected $startTime;
+
+    /**
+     * Current status text
+     *
+     * @var string
+     */
+    protected $statusText = null;
+
+    /**
+     * Adapter for the output
+     *
+     * @var \Zend\ProgressBar\Adapter\AbstractAdapter
+     */
+    protected $adapter;
+
+    /**
+     * Namespace for keeping the progressbar persistent
+     *
+     * @var string
+     */
+    protected $persistenceNamespace = null;
+
+    /**
+     * Create a new progressbar backend.
+     *
+     * @param  Adapter\AbstractAdapter $adapter
+     * @param  float|int               $min
+     * @param  float|int               $max
+     * @param  string|null             $persistenceNamespace
+     * @throws Exception\OutOfRangeException When $min is greater than $max
+     */
+    public function __construct(Adapter\AbstractAdapter $adapter, $min = 0, $max = 100, $persistenceNamespace = null)
+    {
+        // Check min/max values and set them
+        if ($min > $max) {
+            throw new Exception\OutOfRangeException('$max must be greater than $min');
+        }
+
+        $this->min     = (float) $min;
+        $this->max     = (float) $max;
+        $this->current = (float) $min;
+
+        // See if we have to open a session namespace
+        if ($persistenceNamespace !== null) {
+            $this->persistenceNamespace = new Session\Container($persistenceNamespace);
+        }
+
+        // Set adapter
+        $this->adapter = $adapter;
+
+        // Track the start time
+        $this->startTime = time();
+
+        // See If a persistenceNamespace exists and handle accordingly
+        if ($this->persistenceNamespace !== null) {
+            if (isset($this->persistenceNamespace->isSet)) {
+                $this->startTime  = $this->persistenceNamespace->startTime;
+                $this->current    = $this->persistenceNamespace->current;
+                $this->statusText = $this->persistenceNamespace->statusText;
+            } else {
+                $this->persistenceNamespace->isSet      = true;
+                $this->persistenceNamespace->startTime  = $this->startTime;
+                $this->persistenceNamespace->current    = $this->current;
+                $this->persistenceNamespace->statusText = $this->statusText;
+            }
+        } else {
+            $this->update();
+        }
+    }
+
+    /**
+     * Get the current adapter
+     *
+     * @return Adapter\AbstractAdapter
+     */
+    public function getAdapter()
+    {
+        return $this->adapter;
+    }
+
+    /**
+     * Update the progressbar
+     *
+     * @param  float  $value
+     * @param  string $text
+     * @return void
+     */
+    public function update($value = null, $text = null)
+    {
+        // Update value if given
+        if ($value !== null) {
+            $this->current = min($this->max, max($this->min, $value));
+        }
+
+        // Update text if given
+        if ($text !== null) {
+            $this->statusText = $text;
+        }
+
+        // See if we have to update a namespace
+        if ($this->persistenceNamespace !== null) {
+            $this->persistenceNamespace->current    = $this->current;
+            $this->persistenceNamespace->statusText = $this->statusText;
+        }
+
+        // Calculate percent
+        if ($this->min === $this->max) {
+            $percent = false;
+        } else {
+            $percent = (float) ($this->current - $this->min) / ($this->max - $this->min);
+        }
+
+        // Calculate ETA
+        $timeTaken = time() - $this->startTime;
+
+        if ($percent === .0 || $percent === false) {
+            $timeRemaining = null;
+        } else {
+            $timeRemaining = round(((1 / $percent) * $timeTaken) - $timeTaken);
+        }
+
+        // Poll the adapter
+        $this->adapter->notify($this->current, $this->max, $percent, $timeTaken, $timeRemaining, $this->statusText);
+    }
+
+    /**
+     * Update the progressbar to the next value
+     *
+     * @param  int $diff
+     * @param  string $text
+     * @return void
+     */
+    public function next($diff = 1, $text = null)
+    {
+        $this->update(max($this->min, min($this->max, $this->current + $diff)), $text);
+    }
+
+    /**
+     * Call the adapters finish() behaviour
+     *
+     * @return void
+     */
+    public function finish()
+    {
+        if ($this->persistenceNamespace !== null) {
+            unset($this->persistenceNamespace->isSet);
+        }
+
+        $this->adapter->finish();
+    }
+}
+<?php
+/**
  * Project: Digital Edition Curator
  * File: biz.digitalEditionDayRule.php
  *
