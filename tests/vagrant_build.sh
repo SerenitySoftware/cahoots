3990348fcad16a46314c74d2e97ef64b3c5cafed
Hickeroar <ryan.vennell@gmail.com>
2015-03-08 13:45:38 -0500

    Adding some build updates.

--- tests/vagrant_build.sh
+++ tests/vagrant_build.sh
@@ -4,25 +4,25 @@ cd /vagrant
 
 
 echo
-echo "Step 1: Removing old coverage files"
+echo " [Cahoots] Step 1: Removing old coverage files"
 rm -f .coverage*
 
 echo
-echo "Step 2: Executing Unit Tests"
+echo " [Cahoots] Step 2: Executing Unit Tests"
 echo
 nosetests tests/unit/test.py --with-coverage --cover-package=cahoots --cover-min-percentage 100
 echo "Exit Code:" $?
 
 echo
 echo
-echo "Step 3: Executing pep8 and pyflakes Tests (flake8)."
+echo " [Cahoots] Step 3: Executing pep8 and pyflakes Tests (flake8)."
 echo
 flake8 cahoots tests web --benchmark
 echo "Exit Code:" $?
 
 echo
-echo "Step 4: Executing pylint Tests"
+echo " [Cahoots] Step 4: Executing pylint Tests"
 echo
-pylint cahoots tests web
+pylint cahoots tests web --reports=no
 echo "Exit Code:" $?
 echo
