c321e45a3a7d9c6079664ee5a8d43998e5cccea0
Hickeroar <ryan.vennell@gmail.com>
2015-03-10 14:30:25 -0500

    Adding country table and data

---
+++ cahoots/parsers/location/data/country.sql
@@ -0,0 +1,2 @@
+.mode csv
+.import cahoots/parsers/location/data/country.csv country
--- cahoots/parsers/location/data/location.sql
+++ cahoots/parsers/location/data/location.sql
@@ -1,3 +1,26 @@
-.mode csv
-.separator "\t"
-.import cahoots/parsers/location/data/location.txt cities
+CREATE TABLE `city` (
+        `country`       TEXT collate nocase,
+        `postal_code`   TEXT collate nocase,
+        `city`  TEXT collate nocase,
+        `state1`        TEXT collate nocase,
+        `state2`        TEXT collate nocase,
+        `province1`     TEXT,
+        `province2`     TEXT,
+        `community1`    TEXT,
+        `community2`    TEXT,
+        `latitude`      REAL,
+        `longitude`     REAL,
+        `coord_accuracy`        INTEGER
+);
+CREATE INDEX idx_country on city (country collate nocase);
+CREATE INDEX idx_postal_code on city (postal_code collate nocase);
+CREATE INDEX idx_city on city (city collate nocase);
+CREATE INDEX idx_state1 on city (state1 collate nocase);
+CREATE INDEX idx_state2 on city (state2 collate nocase);
+
+CREATE TABLE `country` (
+        `abbreviation`  TEXT collate nocase,
+        `name`  TEXT collate nocase
+);
+CREATE INDEX idx_abbreviation on country (abbreviation collate nocase);
+CREATE INDEX idx_name on country (name collate nocase);
