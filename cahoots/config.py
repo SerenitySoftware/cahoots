dd45dc212f432e53193c064f09f9210373170665
Hickeroar <ryan.vennell@gmail.com>
2015-03-21 17:38:24 -0500

    Removing deprecated junk from the config.

--- cahoots/config.py
+++ cahoots/config.py
@@ -75,27 +75,6 @@ class BaseConfig(object):
         measurement.MeasurementParser,
     ]
 
-    enabledUtilityModules = [
-        LocationDatabase
-    ]
-
-    """
-    Redis config
-        Specify one of the following three:
-        1. host and port
-         - Current values are the redis default
-        2. unix_socket_path
-         - The file path to the unix socket.
-        3. connection_pool
-          - Instance of redis.connection.ConnectionPool
-    """
-    redis = {
-        'host': 'localhost',
-        'port': 6379,
-        'unix_socket_path': None,
-        'connection_pool': None
-    }
-
     """
     Configuration for the templating system
     """
