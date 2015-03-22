fd20e1ef60527a3887844d8d1c87942d4078b52e
Hickeroar <ryan.vennell@gmail.com>
2015-03-21 21:36:23 -0500

    Audit of landmark parser

--- cahoots/parsers/location/address.py
+++ cahoots/parsers/location/address.py
@@ -21,7 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-# pylint: disable=too-many-return-statements
 from cahoots.parsers.base import BaseParser
 from cahoots.parsers.name import NameParser
 from cahoots.parsers.location import \
