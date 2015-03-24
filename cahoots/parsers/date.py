1da7137b0023567201d09cd49adcf03ffb5193d2
Hickeroar <ryan.vennell@gmail.com>
2015-03-24 16:20:47 -0500

    Fixing unhandled exception

--- cahoots/parsers/date.py
+++ cahoots/parsers/date.py
@@ -107,7 +107,7 @@ class DateParser(BaseParser):
         # Checking for other date standards
         try:
             parsed_date = dateUtilParser.parse(data_string)
-        except (StopIteration, ValueError, OverflowError):
+        except (StopIteration, ValueError, OverflowError, TypeError):
             return
 
         punctuation = [c for c in data_string if
