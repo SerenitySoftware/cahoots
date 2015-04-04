b6a8e62d3ec04ad7c927e865e41079eb61de5d95
Hickeroar <ryan.vennell@gmail.com>
2015-04-04 00:28:04 -0500

    Updating version number and changelog

--- setup.py
+++ setup.py
@@ -135,7 +135,7 @@ setup (
         'install': cahoots_install
     },
     name = 'Cahoots',
-    version = '0.3.3',
+    version = '0.3.4',
     url = 'https://github.com/SerenitySoftwareLLC/cahoots',
     maintainer='Serenity Software',
     maintainer_email = 'hello@serenitysoftware.io',
